a99cf7bf59fbcca44811d72b0db1c40d6a8954a9
尼志强 <ni.zhiqiang@gmail.com>
2016-01-14 12:43:56 +0800

    source code

--- build-steno.sh
+++ build-steno.sh
@@ -1,50 +1,45 @@
 #!/bin/sh
 
-HTTPS_PROXY=https://10.0.0.1:8123
-
-SOURCE=/source
-BUILD_DIR=$SOURCE/build
-FINAL_DIR=$SOURCE/final
+BASE_DIR=/source
+BUILD_DIR=$BASE_DIR/build
+FINAL_DIR=$BASE_DIR/final
 
 export GOPATH=${BUILD_DIR}/go
-PKG_USER=github.com/nizq
-PKG_NAME=$PKG_USER/stenographer
-CODE_BASE=$GOPATH/src/$PKG_USER
-STENO_DIR=$GOPATH/src/$PKG_NAME
-STENOTYPE_DIR=${STENO_DIR}/stenotype
+GO_SRC=$GOPATH/src
+VENDORS=$BASE_DIR/src
+
+STENO_USER_DIR=$GO_SRC/github.com/nizq
+STENO_SRC_DIR=$STENO_USER_DIR/stenographer
+STENOTYPE_SRC_DIR=$STENO_SRC_DIR/stenotype
+
+rm -rf $BUILD_DIR/*
+
+mkdir -p $GO_SRC/golang.org/x
+ln -sf $VENDORS/net $GO_SRC/golang.org/x
+
+mkdir -p $STENO_USER_DIR
+ln -sf $VENDORS/stenographer $STENO_SRC_DIR
 
 echo "===> Build testimony..."
-cd $BUILD_DIR
-if [ ! -d "testimony" ]; then
-    git clone https://github.com/google/testimony.git
-fi
-cd testimony/c
+ln -sf $VENDORS/testimony $BUILD_DIR
+cd $BUILD_DIR/testimony/c
 make
 cp -f -v libtestimony.a /usr/lib
 cp -f -v libtestimony.so /usr/lib
 cp -f -v testimony.h /usr/include
 cp -f -v libtestimony.so $FINAL_DIR/
 
-echo "===> Install golang/x/net/context..."
-https_proxy=${HTTPS_PROXY} go get golang.org/x/net/context
-
 echo "===> Install stenographer..."
-rm -rf $CODE_BASE
-mkdir -p $CODE_BASE
-cd $CODE_BASE
-git clone https://${PKG_NAME}.git
-cd $STENO_DIR
-git checkout branch-nizq
 go get github.com/nizq/stenographer
 
 echo "===> Build stenotype manually..."
-cd $STENOTYPE_DIR
+cd $STENOTYPE_SRC_DIR
 sed -i "s/^SHARED_CFLAGS=-std=c++0x -g -Wall -fno-strict-aliasing/SHARED_CFLAGS=-std=c++0x -g -Wall -fno-strict-aliasing -D_GLIBCXX_USE_CXX11_ABI=0/g" Makefile
 sed -i "s/-ltestimony$/-ltestimony -lexecinfo -largp/g" Makefile
 make
 
 echo "===> Collect files ..."
 cp -f -v $GOPATH/bin/stenographer $FINAL_DIR/
-cp -f -v $STENO_DIR/stenocurl $FINAL_DIR/
-cp -f -v $STENO_DIR/stenoread $FINAL_DIR/
-cp -f -v $STENOTYPE_DIR/stenotype $FINAL_DIR/
+cp -f -v $STENO_SRC_DIR/stenocurl $FINAL_DIR/
+cp -f -v $STENO_SRC_DIR/stenoread $FINAL_DIR/
+cp -f -v $STENOTYPE_SRC_DIR/stenotype $FINAL_DIR/
